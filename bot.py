e000f618658e7974220f84c9a2f57eeddfdef8b2
ɢx  ₪ᴇᴏ <89633118+GxNeo@users.noreply.github.com>
2022-02-16 22:23:47 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,6 +7,7 @@ API_HASH = environ.get("API_HASH")
 BOT_TOKEN = environ.get("BOT_TOKEN")
 SESSION = environ.get("SESSION")
 TIME = int(environ.get("TIME"))
+ADMIN = int(environ.get("ADMIN"))
 GROUPS = []
 for grp in environ.get("GROUPS").split():
     GROUPS.append(int(grp))
@@ -31,7 +32,19 @@ Bot = Client(session_name="auto-delete",
              workers=300
              )
 
-
+@Bot.on_message(filters.command('pin') & filters.private)
+async def pin(bot, message):
+    if message.from_user.id == ADMIN: 
+               if message.reply_to_message:
+                                    chatid=int(message.text.replace("/pin"," "))
+                                    p=await bot.copy_message(chat_id=chatid, from_chat_id=ADMIN, message_id=message.reply_to_message.message_id)
+                                    await p.pin()
+                                    await message.reply_text("<b>✅ Message Successfully Send to the Group And pinned</b>")
+               else:
+                    await message.reply_text("<b>Use this command as the reply of any Message to Send in Group</b>")                         
+    else:
+         await message.reply_text("<b>That's not for you bruh 😅</b>")
+            
 @Bot.on_message(filters.command('start') & filters.private)
 async def start(bot, message):
     await message.reply(START_MSG.format(message.from_user.mention))
