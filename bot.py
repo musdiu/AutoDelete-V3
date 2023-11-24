150f5bc279bdf4de168423f8d404e4a018ea98ca
Anonymous么kid <79362039+dangerbots@users.noreply.github.com>
2023-11-24 18:55:13 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,6 +2,7 @@ import asyncio
 from os import environ
 from pyrogram import *
 from pyrogram.types import *
+import time
 
 API_ID = int(environ.get("API_ID"))
 API_HASH = environ.get("API_HASH")
@@ -9,44 +10,18 @@ BOT_TOKEN = environ.get("BOT_TOKEN")
 # SESSION = environ.get("SESSION")
 TIME = int(environ.get("TIME"))
 #ADMIN = int(environ.get("ADMIN"))
-GROUPS = []
-for grp in environ.get("GROUPS").split():
-    GROUPS.append(int(grp))
-ADMINS = []
-for usr in environ.get("ADMINS").split():
-    ADMINS.append(int(usr))
+GROUPS = [int(grp) for grp in environ.get("GROUPS").split()]
+ADMINS = [int(usr) for usr in environ.get("ADMINS").split()]
 
-START_MSG = "<b>Hai {},\nI'm a simple bot to delete group messages after a specific time</b>"
-
-reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("button name", url="http://t.me/@Cinimalokham")]])
-
-# user = Client(name=SESSION,
-#               api_id=API_ID,
-#               api_hash=API_HASH,
-#               workers=300
-#               )
-
+START_MSG = "<b>Hᴇʟʟᴏ {}\n\nI ᴏɴʟʏ sᴜᴘᴘᴏʀᴛ ᴛʜᴇ</b> <a href='http://t.me/Cinimalokham'><b>Cɪɴɪᴍᴀʟᴏᴋʜᴀᴍ</b></a> <b>ɢʀᴏᴜᴘ\n\nI ᴅᴏ ɢʀᴏᴜᴘ ᴀᴜᴛᴏᴍᴀᴛɪᴄ ᴄʟᴇᴀɴɪɴɢ ᴇᴠᴇʀʏ 30 ᴍɪɴᴜᴛᴇs</b>"
+#reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("button name", url="http://t.me/@Cinimalokham")]])
 
 app = Client("auto-delete",
              api_id=API_ID,
              api_hash=API_HASH,
-             bot_token=BOT_TOKEN,
-             workers=300
-             )
+             bot_token=BOT_TOKEN)
 
-#@app.on_message(filters.command('pin') & filters.private)
-#async def pin(bot, message):
-   # if message.from_user.id == ADMIN: 
-               #if message.reply_to_message:
-                                   # chatid=int(message.text.replace("/pin"," "))
-                                    #p=await bot.copy_message(chat_id=chatid, from_chat_id=ADMIN, message_id=message.reply_to_message.message_id)
-                                   #₹ await p.pin()
-                                    #await message.reply_text("<b>✅ Message Successfully Send to the Group And pinned</b>")
-              # else:
-                    #await message.reply_text("<b>Use this command as the reply of any Message to Send in Group</b>")                         
-    #else:
-         #await message.reply_text("<b>That's not for you bruh 😅</b>")
-            
+
 @app.on_message(filters.command('start') & filters.private)
 async def start(__, m: Message):
     await m.reply(START_MSG.format(m.from_user.mention))
@@ -54,13 +29,13 @@ async def start(__, m: Message):
 @app.on_message(filters.chat(GROUPS))
 async def delete(__, m: Message):
     try:
-       if message.from_user.id in ADMINS:
-          return
-       else:
-          await asyncio.sleep(TIME)
-          await app.delete_messages(m.chat.id,m.id)
+        if m.from_user.id in ADMINS:
+            return
+        else:
+            await asyncio.sleep(TIME)
+            await m.delete()
     except Exception as e:
-       print(e)
+        print(e)
        
 app.run()
 print("Bot Started..")
